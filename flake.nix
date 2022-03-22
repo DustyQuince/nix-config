f472a7b85fb9051d56b8a3825554e882817b777b
Dusty Quince <dustyquince@protonmail.com>
2022-03-22 16:20:16 -0600

    locked and features added

--- flake.nix
+++ flake.nix
@@ -1,11 +1,37 @@
 {
   description = "DustyQuince Machine Configuration";
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
+  inputs = {
+    # To update nixpkgs (and thus NixOS), pick the nixos-unstable rev from
+    # https://status.nixos.org/
+    #
+    # This ensures that we always use the official nix cache.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    home-manager.url = "github:nix-community/home-manager";
+    nix-doom-emacs.url = "github:vlaci/nix-doom-emacs";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
   };
+
+  outputs = inputs@{ self, home-manager, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [
+          (import inputs.emacs-overlay)
+        ];
+      };
+    in
+      {
+        nixosConfigurations = {
+          nixos-desktop = nixpkgs.lib.nixosSystem {
+              inherit system pkgs;
+              modules =
+                [
+                  ./features/virtualization.nix
+                ];
+          };
+        };
+      };
 }
