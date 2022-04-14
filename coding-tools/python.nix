34fecedf98ae247d5fbf55c729e6d0e6a92daffd
Dusty Quince <dustyquince@protonmail.com>
2022-04-14 12:23:21 -0600

    python tools

--- coding-tools/python.nix
+++ coding-tools/python.nix
@@ -4,5 +4,11 @@
   environment.systemPackages = with pkgs; [
     python3
     python-language-server
+    black
+    pipenv
+    python39Packages.pyflakes
+    python39Packages.isort
+    python39Packages.nose
+    python39Packages.pytest
   ];
 }
